ad3678790eb9e445505efc9a31bfc14ec5fbd637
ajmainhossain7 <ajmainh0@gmail.com>
2026-06-11 23:04:12 +0600

    Company Profile page api created

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ async function run() {
         const jobCollection = database.collection("jobs");
         const companyCollection = database.collection("companies");
 
-        app.get('/api/jobs', async(req, res) =>{
+        app.get('/api/jobs', async (req, res) => {
             const query = {};
-            if(req.query.companyId){
+            if (req.query.companyId) {
                 query.companyId = req.query.companyId;
             }
-            if(req.query.status){
+            if (req.query.status) {
                 query.status = req.query.status;
             }
             const cursor = jobCollection.find(query);
@@ -55,6 +55,23 @@ async function run() {
         app.post('/api/jobs', async (req, res) => {
             const job = req.body;
             const result = await jobCollection.insertOne(job);
+            res.send(result);
+        })
+
+
+        // Company Related Apis
+        app.get('api/my/companies', async (req, res) => {
+            const query = {};
+            if(req.query.recruiterId){
+                query.recruiterId = req.query.recruiterId;
+            }
+            const result = await companyCollection.findOne(query)
+            res.send(result);
+        })
+
+        app.post('api/companies', async (req, res) => {
+            const company = req.body;
+            const result = await companyCollection.insertOne(company);
             res.send(result);
         })
 
