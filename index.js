d8158efdfb577e645d4a56a3f354dddd113f6a41
ajmainhossain7 <ajmainh0@gmail.com>
2026-06-11 23:46:49 +0600

    Control null value when company profile is empty

--- index.js
+++ index.js
@@ -54,7 +54,11 @@ async function run() {
 
         app.post('/api/jobs', async (req, res) => {
             const job = req.body;
-            const result = await jobCollection.insertOne(job);
+            const newJob = {
+                ...job,
+                careatedAt: new Date()
+            }
+            const result = await jobCollection.insertOne(newJob);
             res.send(result);
         })
 
@@ -66,12 +70,16 @@ async function run() {
                 query.recruiterId = req.query.recruiterId;
             }
             const result = await companyCollection.findOne(query)
-            res.send(result);
+            res.send(result || {});
         })
 
         app.post('api/companies', async (req, res) => {
             const company = req.body;
-            const result = await companyCollection.insertOne(company);
+            const newCompany = {
+                ...company,
+                careatedAt: new Date()
+            }
+            const result = await companyCollection.insertOne(newCompany);
             res.send(result);
         })
 
