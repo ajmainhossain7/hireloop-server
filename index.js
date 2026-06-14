cf2e121c52ec63a2def7e006dade529cd50c49ff
ajmainhossain7 <ajmainh0@gmail.com>
2026-06-14 16:01:04 +0600

    Apply page error fix{correction collection name applicationCollection to applicationsCollection

--- index.js
+++ index.js
@@ -35,12 +35,21 @@ async function run() {
         await client.connect();
 
 
-        const database = client.db("hireloop_db");
+        const database = client.db("hireloopdb");
         const jobCollection = database.collection("jobs");
         const companyCollection = database.collection("companies");
-        const applicationCollection = database.collection("applications");
+        const usersCollection = database.collection("user");
+        const applicationsCollection = database.collection("applications");
+        const planCollection = database.collection("plans");
+        const subscriptionCollection = database.collection("subscriptions");
 
-        // job related apis
+
+        app.get('/api/users', async (req, res) => {
+
+            const cursor = usersCollection.find().skip(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
         app.get('/api/jobs', async (req, res) => {
             const query = {};
@@ -55,36 +64,6 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/api/jobs', async (req, res) => {
-            const job = req.body;
-            const newJob = {
-                ...job,
-                careatedAt: new Date()
-            }
-            const result = await jobCollection.insertOne(newJob);
-            res.send(result);
-        })
-
-        // application related apis
-
-        app.post('/api/applications', async (req, res) => {
-            const application = req.body;
-            const newApplication = {
-                ...application,
-                createdAt: new Date()
-            };
-            const result = await applicationCollection.insertOne(newApplication);
-            res.send(result);
-        });
-
-
-        // Company Related Apis
-        app.get('/api/companies' , async (req, res) => {
-            const cursor = companyCollection.find();
-            const result = await cursor.toArray();
-            res.send(result)
-        })
-
         app.get('/api/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
@@ -94,12 +73,55 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/api/jobs', async (req, res) => {
+            const job = req.body;
+            const newJob = {
+                ...job,
+                createdAt: new Date()
+            }
+            const result = await jobCollection.insertOne(newJob);
+            res.send(result);
+        })
+
+        // application related apis
+        app.get('/api/applications', async (req, res) => {
+            const query = {};
+            if (req.query.applicantId) {
+                query.applicantId = req.query.applicantId;
+            }
+            if (req.query.jobId) {
+                query.jobId = req.query.jobId;
+            }
+            const cursor = applicationsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/api/applications', async (req, res) => {
+            const application = req.body;
+            const newApplication = {
+                ...application,
+                createdAt: new Date()
+            }
+            const result = await applicationsCollection.insertOne(newApplication);
+            res.send(result);
+        })
+
+        // company related apis
+        app.get('/api/companies', async (req, res) => {
+            const cursor = companyCollection.find().skip(4);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
         app.get('/api/my/companies', async (req, res) => {
             const query = {};
-            if(req.query.recruiterId){
+            if (req.query.recruiterId) {
                 query.recruiterId = req.query.recruiterId;
             }
-            const result = await companyCollection.findOne(query)
+            const result = await companyCollection.findOne(query);
+
             res.send(result || {});
         })
 
@@ -107,11 +129,168 @@ async function run() {
             const company = req.body;
             const newCompany = {
                 ...company,
-                careatedAt: new Date()
+                createdAt: new Date()
             }
             const result = await companyCollection.insertOne(newCompany);
             res.send(result);
         })
+
+        // plans 
+        app.get('/api/plans', async (req, res) => {
+            const query = {}
+            if (req.query.plan_id) {
+                query.id = req.query.plan_id
+            }
+            const plan = await planCollection.findOne(query);
+            res.send(plan)
+        })
+
+        // subscription 
+        app.post('/api/subscriptions', async (req, res) => {
+            const data = req.body;
+            const subsInfo = {
+                ...data,
+                createdAt: new Date()
+            }
+
+            const result = await subscriptionCollection.insertOne(subsInfo);
+
+            // update the user plan information
+            const filter = { email: data.email };
+            // update the value of the 'quantity' field to 5
+            const updateDocument = {
+                $set: {
+                    plan: data.planId,
+                },
+            };
+
+            const updateResult = await usersCollection.updateOne(filter, updateDocument);
+            res.send(updateResult)
+        })
+
+
+        // app.get('/api/users', async (req, res) => {
+
+        //     const cursor = usersCollection.find().skip(6);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+        // // job related apis
+
+        // app.get('/api/jobs', async (req, res) => {
+        //     const query = {};
+        //     if (req.query.companyId) {
+        //         query.companyId = req.query.companyId;
+        //     }
+        //     if (req.query.status) {
+        //         query.status = req.query.status;
+        //     }
+        //     const cursor = jobCollection.find(query);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+
+        // app.post('/api/jobs', async (req, res) => {
+        //     const job = req.body;
+        //     const newJob = {
+        //         ...job,
+        //         careatedAt: new Date()
+        //     }
+        //     const result = await jobCollection.insertOne(newJob);
+        //     res.send(result);
+        // })
+
+        // app.get('/api/jobs/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = {
+        //         _id: new ObjectId(id)
+        //     }
+        //     const result = await jobCollection.findOne(query);
+        //     res.send(result);
+        // })
+
+        // // application related apis
+
+        // app.get('/api/applications', async (req, res) => {
+        //     const query = {};
+        //     if (req.query.applicantId) {
+        //         query.applicantId = req.query.applicantId;
+        //     }
+        //     if (req.query.jobId) {
+        //         query.jobId = req.query.jobId;
+        //     }
+        //     const cursor = applicationsCollection.find(query);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
+
+        // app.post('/api/applications', async (req, res) => {
+        //     const application = req.body;
+        //     const newApplication = {
+        //         ...application,
+        //         createdAt: new Date()
+        //     }
+        //     const result = await applicationsCollection.insertOne(newApplication);
+        //     res.send(result);
+        // })
+
+        // // Company Related Apis
+        // app.get('/api/companies' , async (req, res) => {
+        //     const cursor = companyCollection.find();
+        //     const result = await cursor.toArray();
+        //     res.send(result)
+        // })
+
+        // app.get('/api/my/companies', async (req, res) => {
+        //     const query = {};
+        //     if(req.query.recruiterId){
+        //         query.recruiterId = req.query.recruiterId;
+        //     }
+        //     const result = await companyCollection.findOne(query)
+        //     res.send(result || {});
+        // })
+
+        // app.post('/api/companies', async (req, res) => {
+        //     const company = req.body;
+        //     const newCompany = {
+        //         ...company,
+        //         careatedAt: new Date()
+        //     }
+        //     const result = await companyCollection.insertOne(newCompany);
+        //     res.send(result);
+        // })
+
+        // // plans related apis
+        // app.get('/api/plans', async (req, res) => {
+        //     const query = {}
+        //     if (req.query.plan_id) {
+        //         query.id = req.query.plan_id
+        //     }
+        //     const plan = await planCollection.findOne(query);
+        //     res.send(plan)
+        // })
+
+        // app.post('/api/subscriptions', async (req, res) => {
+        //     const data = req.body;
+        //     const subsInfo = {
+        //         ...data,
+        //         createdAt: new Date()
+        //     }
+
+        //     const result = await subscriptionCollection.insertOne(subsInfo);
+
+        //     // update the user plan information
+        //     const filter = { email: data.email };
+        //     // update the value of the 'quantity' field to 5
+        //     const updateDocument = {
+        //         $set: {
+        //             plan: data.planId,
+        //         },
+        //     };
+
+        //     const updateResult = await usersCollection.updateOne(filter, updateDocument);
+        //     res.send(updateResult)
+        // })
 
 
         // Send a ping to confirm a successful connection
